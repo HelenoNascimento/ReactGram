8301c19123a210c8d74552ac5c7a645d64bd462c
Heleno Nascimento <heleno_55@hotmail.com>
2022-08-05 12:04:55 -0300

    iniciando controle de imagens

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -4,6 +4,8 @@ const User = require("../models/User");
 const bcrypt = require ("bcryptjs");
 const jwt = require("jsonwebtoken");
 
+const mongoose = require("mongoose");
+
 const jwtSecret = process.env.JWT_SECRET;
 
 // generate user Token
@@ -85,8 +87,45 @@ const register = async(req,res) =>{
                 res.status(200).json(user);
             }
 
+            //update an user
+
+            const update = async(req, res) =>{
+                const { name, password, bio} = req.body
+                let profileImage = null;
+
+                if(req.file){
+                    profileImage = req.file.filename
+                }
+
+                const reqUser = req.user
+
+                const user = await User.findById(mongoose.Types.ObjectId(reqUser._id)).select("-password");
+
+                if(name){
+                    user.name = name
+                }
+                if(password){
+                   // Generate password hash
+                    const salt = await bcrypt.genSalt();
+                    const passwordHash = await bcrypt.hash(password, salt);
+
+                    user.password = passwordHash
+                }
+
+                if(profileImage){
+                    user.profileImage = profileImage
+                }
+                if(bio){
+                    user.bio = bio
+                }
+
+                await user.save()
+                res.status(200).json(user);
+            };
+
 module.exports ={
     register,
     login,
     getCurrentUser,
+    update,
 };
--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -2,12 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 //controller
-const { register, login, getCurrentUser } = require("../controllers/UserController");
+const { register, login, getCurrentUser,update } = require("../controllers/UserController");
 
 //middlewares
 const validate = require("../middlewares/handleValidation");
-const { userCreateValidation, loginValidation} = require("../middlewares/UseValidations");
+const { userCreateValidation, loginValidation, userUpdateValidation} = require("../middlewares/UseValidations");
 const authGuard = require("../middlewares/authGuard");
+const { imageUpload } = require("../middlewares/ImageUpload");
+//const { update } = require("../models/User");
 
 
 //routes
@@ -15,5 +17,6 @@ const authGuard = require("../middlewares/authGuard");
 router.post("/register", userCreateValidation(),validate, register);
 router.post("/login", loginValidation(),validate, login);
 router.get("/profile", authGuard, getCurrentUser);
+router.put("/", authGuard, userUpdateValidation(), validate, imageUpload.single("profileImage"), update)
 
 module.exports = router;
