0cdd51f896a72de9f9163711679dfb9d05ff6703
Heleno Nascimento <heleno_55@hotmail.com>
2022-08-08 16:51:57 -0300

    finalizado funcionalidades basicas do backend

--- backend/routes/PhotoRoutes.js
+++ backend/routes/PhotoRoutes.js
@@ -2,10 +2,10 @@ const express = require("express")
 const router = express.Router();
 
 //controller
-const {insertPhoto, deletePhoto, getAllPhotos, getUserPhotos, getPhotoById, updatePhoto} = require("../controllers/PhotoController")
+const {insertPhoto, deletePhoto, getAllPhotos, getUserPhotos, getPhotoById, updatePhoto, likePhoto, commentPhoto, searchPhoto} = require("../controllers/PhotoController")
 
 //middlewares
-const{ photoInsertValidation, photoUpdateValidation} = require("../middlewares/photoValidation");
+const{ photoInsertValidation, photoUpdateValidation, commentValidation} = require("../middlewares/photoValidation");
 const authGuard = require("../middlewares/authGuard");
 const validate = require("../middlewares/handleValidation");
 const { imageUpload} = require("../middlewares/imageUpload")
@@ -19,8 +19,16 @@ router.get("/", authGuard, getAllPhotos);
 
 router.get("/user/:id", authGuard, getUserPhotos);
 
+router.get("/search", authGuard, searchPhoto);
+
 router.get("/:id", authGuard, getPhotoById);
 
 router.put("/:id", authGuard, photoUpdateValidation(), validate, updatePhoto);
+
+router.put("/like/:id", authGuard, likePhoto);
+
+router.put("/comment/:id",authGuard, commentValidation(), validate, commentPhoto);
+
+
 
 module.exports = router;
