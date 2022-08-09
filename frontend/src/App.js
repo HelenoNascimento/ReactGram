75f6e0a2796f42fa1b692c48cb4c1779096e37d2
Heleno Nascimento <heleno_55@hotmail.com>
2022-08-08 23:39:33 -0300

    iniciando o frontend tela de cadastro finalizada

---
+++ frontend/src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+
+//Router
+import { BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
+
+//components
+import Navbar from "./components/Navbar"
+import Footer from './components/Footer';
+
+
+import Home from './pages/Home/Home';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Navbar />
+       <div className="container">
+       <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/register" element={<Register />}/>
+        </Routes>
+       </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
