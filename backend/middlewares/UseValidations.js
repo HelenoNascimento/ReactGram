3bbc48b6796ed7dd0151ae8bfa357a96a32d6abd
Heleno Nascimento <heleno_55@hotmail.com>
2022-08-09 17:25:51 -0300

    finalizado tela e funcionalidade para cadastro de usuario

--- backend/middlewares/UseValidations.js
+++ backend/middlewares/UseValidations.js
@@ -17,7 +17,7 @@ const userCreateValidation = () =>{
             .withMessage("A senha é obrigatorio.")
             .isLength({min:5})
             .withMessage(" A senha precisa ter no minimo 5 caracteres."),
-        body("confirmpassword")
+        body("confirmPassword")
             .isString()
             .withMessage("A confirmação de senha é obrigatorio")
             .custom((value,{ req})=> {
